f74e4fa3a1c1a11c11c1a9cb1aa465111d9a3c4f
jw022025 <jw@aigenixsolutions.com>
2025-09-09 23:23:11 -0400

    Add minimal test function to diagnose 502 errors

---
+++ netlify/functions/test-simple.js
@@ -0,0 +1,13 @@
+export const handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      message: 'Simple test function works!',
+      timestamp: new Date().toISOString()
+    })
+  };
+};
